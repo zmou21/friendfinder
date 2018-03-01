8c22409f8e7c6a83c226876485e73033aa1cf3db
Zack <zmoumen13@gmail.com>
2018-02-28 23:11:04 -0700

    updated into dynamic port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 
   // Sets up the Express App
   var app = express();
-  var PORT = 3000;
+  var port = process.env.PORT || 3000;
 
   // Sets up the Express app to handle data parsing
   app.use(bodyParser.urlencoded({ extended: false }));
@@ -120,8 +120,8 @@
   };
 
   //The below code effectively "starts" our server
-  app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+  app.listen(port, function() {
+    console.log("App listening on PORT: " + port);
   });
 
 
